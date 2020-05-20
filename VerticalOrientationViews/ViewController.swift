e49cb12e44c7340bf915940ec2d6684a69e08091
Olegstasiw <Olegstasiw@icloud.com>
2020-05-20 11:34:30 +0300

    Adding 2 views

---
+++ VerticalOrientationViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VerticalOrientationViews
+//
+//  Created by mac on 20.05.2020.
+//  Copyright © 2020 Oleg Stasiw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
